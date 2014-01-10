2f9e16142fcde4493670abc2000460b96d46fd00
Paul Frazee <pfrazee@gmail.com>
2014-01-10 16:45:03 -0600

    improved front page instructions

--- frontend_src/dashboard/main.js
+++ frontend_src/dashboard/main.js
@@ -19,7 +19,7 @@ common.feedUA = local.agent('httpl://feed');
 
 common.feedUA.POST('Welcome to Grimwire v0.6 unstable build. Please report any bugs or complaints to our <a href="https://github.com/grimwire/grimwire/issues" target="_blank">issue tracker</a>.', { Content_Type: 'text/html' });
 common.feedUA.POST('<small class=text-muted>Early Beta Build. Not all behaviors are expected.</small>', {Content_Type: 'text/html'});
-common.feedUA.POST('<div style="padding: 10px 0"><img src="/img/exclamation.png" style="position: relative; top: -2px"> You should explore!</div>', { Content_Type: 'text/html' });
+common.feedUA.POST('<div style="padding: 10px 0"><img src="/img/exclamation.png" style="position: relative; top: -2px"> You should <a href="httpl://explorer" target="_content">explore</a> and <a href="httpl://explorer/intro" target="_content">read the intro</a>.</div>', { Content_Type: 'text/html' });
 common.dispatchRequest({ method: 'GET', url: /*window.location.hash.slice(1) || */'feed', target: '_content' });
 
 // So PouchDB can target locals
