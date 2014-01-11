cc49130c99f5502d0b2f9a8ad5fbe903165f3aba
Paul Frazee <pfrazee@gmail.com>
2014-01-11 13:31:37 -0600

    added worker and online global nav buttons

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -925,6 +925,12 @@ $('#change-email').on('click', function() {
 	return false;
 });
 
+// :DEBUG:
+// Show online button
+$('.show-online').on('click', function() {
+	common.layout.toggle('east');
+});
+
 // Change password link
 $('#change-pw').on('click', function() {
 	if (!_session_user) return false;
