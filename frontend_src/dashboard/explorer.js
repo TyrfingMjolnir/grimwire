f13fa2429b7471670f05c82975d0085f52db2270
Paul Frazee <pfrazee@gmail.com>
2014-01-19 18:34:04 -0600

    fix: explorer now correctly sets From on upstream request

--- frontend_src/dashboard/explorer.js
+++ frontend_src/dashboard/explorer.js
@@ -43,7 +43,7 @@ server.route('/', function(link, method) {
 			}
 		});
 		uri = uritmpl.expand(ctx);
-		local.HEAD({ url: uri, From: 'explorer' }).always(function(res2) {
+		local.HEAD({ url: uri, From: 'httpl://explorer' }).always(function(res2) {
 			// Build explore interface
 			var links = (res2.parsedHeaders.link) ? res2.parsedHeaders.link : [];
 			var viaLink = local.queryLinks(links, { rel: 'via !up !self' })[0];
