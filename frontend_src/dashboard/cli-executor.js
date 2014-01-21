5b98821fdb5ce7fe7757bbae0dc445536da9e83a
Paul Frazee <pfrazee@gmail.com>
2014-01-21 11:18:12 -0600

    added type aliases to cli pipe

--- frontend_src/dashboard/cli-executor.js
+++ frontend_src/dashboard/cli-executor.js
@@ -40,7 +40,7 @@ function onRequest(e) {
 	var body;
 	var req = new local.Request(e.request);
 	// pull accept from right-side pipe
-	if (e.pipe && !e.request.accept) { req.header('Accept', e.pipe); }
+	if (e.pipe && !e.request.accept) { req.header('Accept', pipeToType(e.pipe)); }
 	// pull body and content-type from left-side pipe (last request)
 	if (e.last_res) {
 		if (e.last_res.header('Content-Type') && !req.header('Content-Type')) {
@@ -73,4 +73,14 @@ function onResponse(e) {
 		next_cmd.last_res = e.response;
 	}
 	local.util.nextTick(this.fireNext.bind(this));
+}
+
+var pipeMap = {
+	html: 'text/html',
+	text: 'text/plain',
+	plain: 'text/plain',
+	json: 'application/json'
+};
+function pipeToType(v) {
+	return pipeMap[v] || v;
 }
